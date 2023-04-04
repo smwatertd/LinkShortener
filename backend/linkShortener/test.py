4e8c9b3d85d0db115726be9a2641c4b62e95a902
smwatertd <korablev.danil.smtd@mail.ru>
2023-04-05 00:59:36 +0500

    change api factory to api client & update tests

--- backend/linkShortener/test.py
+++ backend/linkShortener/test.py
@@ -1,21 +1,30 @@
 from django.contrib.auth.models import User
+from django.urls import reverse
 
-import linkShortener.views as views
 from linkShortener.models import Socket
 
 from rest_framework.test import (
-    APIRequestFactory,
+    APIClient,
     APITestCase,
-    force_authenticate,
 )
+
+from rest_framework_simplejwt.tokens import AccessToken
 
 
 class TestSockets(APITestCase):
     def setUp(self):
-        example_full_url = 'https://www.youtube.com/'
-        self.create_socket_url = '/api/v1/sockets/'
-        self.request_data = {
-            'full_url': example_full_url,
+        self.short_url = 'abcdef'
+
+        self.create_socket_url = reverse('create_sockets')
+        self.user_sockets_url = reverse('get_user_sockets')
+        self.redirect_url = reverse('redirect', kwargs={'short_url': 'abcdef'})
+
+        self.success_request_data = {
+            'full_url': 'https://www.youtube.com/',
+        }
+
+        self.failure_request_data = {
+            'full_url': 'abc',
         }
 
         self.user = User.objects.create_user(
@@ -23,96 +32,96 @@ class TestSockets(APITestCase):
             'admin@gmail.com',
             'adminpassword',
         )
-        self.factory = APIRequestFactory()
+        self.client = APIClient()
+        self.access_token = AccessToken.for_user(self.user)
+
+    def auth_client(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
 
     def test_success_auth_create_socket(self):
-        request = self.factory.post(self.create_socket_url, self.request_data, format='json')
-        force_authenticate(request, user=self.user)
+        self.auth_client()
 
-        response = views.CreateSocketView.as_view()(request)
-        created_socket = Socket.objects.get(full_url=self.request_data['full_url'])
+        response = self.client.post(
+            self.create_socket_url,
+            self.success_request_data,
+            format='json',
+        )
 
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(created_socket.full_url, self.request_data['full_url'])
+        created_socket = Socket.objects.get(full_url=self.success_request_data['full_url'])
+        self.assertEqual(response.status_code, 201, msg=self.create_socket_url)
+        self.assertEqual(created_socket.full_url, self.success_request_data['full_url'])
         self.assertIsNotNone(created_socket.short_url)
         self.assertEqual(created_socket.author, self.user)
 
     def test_failure_auth_create_socket(self):
-        failure_example_full_url = 'abc'
-        request = self.factory.post(
+        self.auth_client()
+
+        response = self.client.post(
             self.create_socket_url,
-            {'full_url': failure_example_full_url},
+            self.failure_request_data,
             format='json',
         )
-        force_authenticate(request, user=self.user)
-
-        response = views.CreateSocketView.as_view()(request)
 
         self.assertEqual(response.status_code, 400)
         with self.assertRaises(Socket.DoesNotExist):
-            Socket.objects.get(full_url=failure_example_full_url)
+            Socket.objects.get(full_url=self.failure_request_data['full_url'])
 
     def test_success_anonymous_create_socket(self):
-        request = self.factory.post(self.create_socket_url, self.request_data, format='json')
+        response = self.client.post(
+            self.create_socket_url,
+            self.success_request_data,
+            format='json',
+        )
 
-        response = views.CreateSocketView.as_view()(request)
-        created_socket = Socket.objects.get(full_url=self.request_data['full_url'])
-
+        created_socket = Socket.objects.get(full_url=self.success_request_data['full_url'])
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(created_socket.full_url, self.request_data['full_url'])
+        self.assertEqual(created_socket.full_url, self.success_request_data['full_url'])
         self.assertIsNotNone(created_socket.short_url)
 
     def test_failure_anonymous_create_socket(self):
-        failure_example_full_url = 'abc'
-        request = self.factory.post(
+        response = self.client.post(
             self.create_socket_url,
-            {'full_url': failure_example_full_url},
+            self.failure_request_data,
             format='json',
         )
 
-        response = views.CreateSocketView.as_view()(request)
-
         self.assertEqual(response.status_code, 400)
         with self.assertRaises(Socket.DoesNotExist):
-            Socket.objects.get(full_url=failure_example_full_url)
+            Socket.objects.get(full_url=self.failure_request_data['full_url'])
 
     def test_success_redirection(self):
-        short_url = 'abcdef'
         Socket.objects.create(
             author=None,
-            full_url=self.request_data['full_url'],
-            short_url=short_url,
+            full_url=self.success_request_data['full_url'],
+            short_url=self.short_url,
         )
-        request = self.factory.get('/%s/' % short_url)
-        response = views.RedirectionView.as_view()(request, short_url=short_url)
 
-        self.assertEqual(response.status_code, 302)
+        response = self.client.get(self.redirect_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['full_url'], self.success_request_data['full_url'])
 
     def test_failure_redirection(self):
-        short_url = 'abcdef'
+        response = self.client.get(self.redirect_url)
 
-        request = self.factory.get('/%s/' % short_url)
-        response = views.RedirectionView.as_view()(request, short_url=short_url)
-
-        self.assertEqual(response.status_code, 410)
+        self.assertEqual(response.status_code, 404)
 
     def test_success_get_user_socket_list(self):
-        short_url = 'abcdef'
+        self.auth_client()
         Socket.objects.create(
             author=self.user,
-            full_url=self.request_data['full_url'],
-            short_url=short_url,
+            full_url=self.success_request_data['full_url'],
+            short_url=self.short_url,
         )
 
-        request = self.factory.get('/api/v1/users/')
-        force_authenticate(request, user=self.user)
-        response = views.UserSocketsView.as_view()(request)
+        response = self.client.get(
+            self.user_sockets_url,
+        )
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 1)
 
     def test_failure_get_user_socket_list(self):
-        request = self.factory.get('/api/v1/users/')
-        response = views.UserSocketsView.as_view()(request)
+        response = self.client.get(self.user_sockets_url)
 
         self.assertEqual(response.status_code, 401)
