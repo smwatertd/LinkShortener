50e1fb2577d7294ec80bddf22e00100bf7301fbb
smwatertd <korablev.danil.smtd@mail.ru>
2023-04-05 00:59:14 +0500

    update urls

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('admin/', admin.site.urls, name='admin'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('', include('linkShortener.urls')),
 ]
