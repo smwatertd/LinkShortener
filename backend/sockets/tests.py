b3763c30ecd82f307142bc264c9206b73890e97c
smwatertd <korablev.danil.smtd@mail.ru>
2023-05-09 01:45:28 +0500

    update tests

--- backend/sockets/tests.py
+++ backend/sockets/tests.py
@@ -10,27 +10,21 @@ from sockets.models import Socket
 
 class TestSockets(APITestCase):
     def setUp(self):
-        # Data
+        # Данные
         self.correct_short_url = 'correct_short_url'
         self.incorrect_short_url = 'incorrect_short_url'
         self.correct_full_url = 'https://www.example.com/'
         self.incorrect_full_url = 'wrong_full_url'
         self.correct_request_data = {'full_url': self.correct_full_url}
         self.incorrect_request_data = {'full_url': self.incorrect_full_url}
+        self.user = self._create_user()
+
+        # API клиент
         self.client = APIClient()
 
-        User = get_user_model()
-        self.user = User.objects.create(
-            username='admin',
-            email='admin@gmail.com',
-            password='adminpassword',
-        )
-        self.access_token = AccessToken.for_user(self.user)
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
-
         # Urls
-        self.create_socket_url = reverse('create_sockets')
-        self.user_sockets_url = reverse('user_view')
+        self.create_socket_url = reverse('create_socket')
+        self.user_url = reverse('users')
         self.redirect_url = reverse(
             'redirect',
             kwargs={'short_url': self.correct_short_url},
@@ -40,7 +34,28 @@ class TestSockets(APITestCase):
             kwargs={'pk': self.correct_short_url},
         )
 
-    def test_success_create_socket(self):
+    def _create_user(self, **kwargs):
+        """
+        Создание тестового пользователя
+        """
+        kwargs.setdefault('username', 'admin')
+        kwargs.setdefault('email', 'admin@gmail.com')
+        kwargs.setdefault('password', 'adminPassword')
+        User = get_user_model()
+        user = User.objects.create(**kwargs)
+        return user
+
+    def _set_client_credentials(self):
+        """
+        Установка заголовка авторизации для API клиента
+        """
+        access_token = AccessToken.for_user(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+
+    def test_success_not_auth_create_socket(self):
+        """
+        Успешное создание сокета без автора
+        """
         response = self.client.post(
             self.create_socket_url,
             self.correct_request_data,
@@ -49,9 +64,14 @@ class TestSockets(APITestCase):
         socket = Socket.objects.get(full_url=self.correct_full_url)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(socket.full_url, self.correct_full_url)
+        self.assertEqual(socket.author, None)
         self.assertIsNotNone(socket.short_url)
 
-    def test_failure_create_socket(self):
+    def test_failure_not_auth_create_socket(self):
+        """
+        Неуспешное создание сокета без автора.
+        Причина: неправильный полный url
+        """
         response = self.client.post(
             self.create_socket_url,
             self.incorrect_request_data,
@@ -62,8 +82,10 @@ class TestSockets(APITestCase):
             Socket.objects.get(full_url=self.incorrect_full_url)
 
     def test_success_redirection(self):
+        """
+        Успешное получение полного url по короткому
+        """
         socket = Socket.objects.create(
-            author=None,
             full_url=self.correct_full_url,
             short_url=self.correct_short_url,
         )
@@ -75,23 +97,36 @@ class TestSockets(APITestCase):
         self.assertEqual(socket.views.count(), 1)
 
     def test_failure_redirection(self):
+        """
+        Неуспешное получение полного url по короткому.
+        Причина: сокет не существует
+        """
         response = self.client.get(self.redirect_url)
 
         self.assertEqual(response.status_code, 404)
 
     def test_success_delete_socket(self):
+        """
+        Успешное удаление сокета
+        """
         Socket.objects.create(
             author=self.user,
             full_url=self.correct_full_url,
             short_url=self.correct_short_url,
         )
+        self._set_client_credentials()
 
         response = self.client.delete(self.delete_socket_url)
 
+        sockets_count = Socket.objects.filter(short_url=self.correct_short_url).count()
         self.assertEqual(response.status_code, 204)
-        self.assertEqual(Socket.objects.filter(short_url=self.correct_short_url).count(), 0)
+        self.assertEqual(sockets_count, 0)
 
     def test_failure_wrong_url_delete_socket(self):
+        """
+        Неудачное удаление сокета.
+        Причина: неправильный короткий url
+        """
         Socket.objects.create(
             author=self.user,
             full_url=self.correct_full_url,
@@ -100,23 +135,29 @@ class TestSockets(APITestCase):
 
         response = self.client.delete(self.delete_socket_url)
 
+        sockets_count = Socket.objects.filter(short_url=self.incorrect_short_url).count()
         self.assertEqual(response.status_code, 204)
-        self.assertEqual(Socket.objects.filter(short_url=self.incorrect_short_url).count(), 1)
+        self.assertEqual(sockets_count, 1)
 
     def test_failure_not_author_delete_socket(self):
-        User = get_user_model()
-        user = User.objects.create(
+        """
+        Неудачное удаление сокета.
+        Причина: у сокета другой автор
+        """
+        other_user = self._create_user(
             username='user',
             email='user@gmail.com',
-            password='password',
+            password='userPassword',
         )
         Socket.objects.create(
-            author=user,
+            author=other_user,
             full_url=self.correct_full_url,
             short_url=self.correct_short_url,
         )
+        self._set_client_credentials()
 
         response = self.client.delete(self.delete_socket_url)
 
+        sockets_count = Socket.objects.filter(short_url=self.correct_short_url).count()
         self.assertEqual(response.status_code, 204)
-        self.assertEqual(Socket.objects.filter(short_url=self.correct_short_url).count(), 1)
+        self.assertEqual(sockets_count, 1)
--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
+from parameterized import parameterized
+
 from rest_framework.test import APIClient, APITestCase
 
 from rest_framework_simplejwt.tokens import AccessToken
@@ -10,41 +12,74 @@ from sockets.models import Socket
 
 class TestSockets(APITestCase):
     def setUp(self):
-        # Data
+        # Данные
         self.correct_short_url = 'correct_short_url'
         self.incorrect_short_url = 'incorrect_short_url'
         self.correct_full_url = 'https://www.example.com/'
         self.incorrect_full_url = 'incorrect_full_url'
         self.correct_request_data = {'full_url': self.correct_full_url}
         self.incorrect_request_data = {'full_url': self.incorrect_full_url}
-        self.client = APIClient()
+        self.user = self._create_user()
 
+        # API клиент
+        self.client = APIClient()
+        self._set_client_credentials()
+
+        # Urls
+        self.create_socket_url = reverse('create_socket')
+        self.user_sockets_url = reverse('users')
+
+    def _create_user(self, **kwargs):
+        """
+        Создание тестового пользователя
+        """
+        kwargs.setdefault('username', 'admin')
+        kwargs.setdefault('email', 'admin@gmail.com')
+        kwargs.setdefault('password', 'adminPassword')
         User = get_user_model()
-        self.user = User.objects.create(
-            username='admin',
-            email='admin@gmail.com',
-            password='adminpassword',
-        )
+        user = User.objects.create(**kwargs)
+        return user
+
+    def _set_client_credentials(self):
+        """
+        Установка заголовка авторизации для API клиента
+        """
         access_token = AccessToken.for_user(self.user)
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
 
-        # Urls
-        self.create_socket_url = reverse('create_sockets')
-        self.user_sockets_url = reverse('user_view')
+    def _logout(self):
+        """
+        Деавторизация API клиента
+        """
+        self.client.logout()
 
-    def test_success_create_socket(self):
+    def _get_users_count(self):
+        """
+        Получение количество пользователей
+        """
+        User = get_user_model()
+        return User.objects.all().count()
+
+    def test_success_auth_create_socket(self):
+        """
+        Успешное создание сокета с автором
+        """
         response = self.client.post(
             self.create_socket_url,
             self.correct_request_data,
         )
 
         created_socket = Socket.objects.get(full_url=self.correct_full_url)
-        self.assertEqual(response.status_code, 201, msg=self.create_socket_url)
+        self.assertEqual(response.status_code, 201)
         self.assertEqual(created_socket.full_url, self.correct_full_url)
-        self.assertIsNotNone(created_socket.short_url)
         self.assertEqual(created_socket.author, self.user)
+        self.assertIsNotNone(created_socket.short_url)
 
-    def test_failure_create_socket(self):
+    def test_failure_auth_create_socket(self):
+        """
+        Неуспешное создание сокета с автором
+        Причина: неправильный полный url
+        """
         response = self.client.post(
             self.create_socket_url,
             self.incorrect_request_data,
@@ -54,7 +89,10 @@ class TestSockets(APITestCase):
         with self.assertRaises(Socket.DoesNotExist):
             Socket.objects.get(full_url=self.incorrect_full_url)
 
-    def test_success_get_user_socket_list(self):
+    def test_success_get_user_sockets(self):
+        """
+        Успешное получение сокетов пользователя
+        """
         Socket.objects.create(
             author=self.user,
             full_url=self.correct_full_url,
@@ -63,11 +101,72 @@ class TestSockets(APITestCase):
 
         response = self.client.get(self.user_sockets_url)
 
+        sockets_count = len(response.data)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 1)
+        self.assertEqual(sockets_count, 1)
 
-    def test_failure_get_user_socket_list(self):
-        self.client.logout()
+    def test_failure_not_auth_get_user_sockets(self):
+        """
+        Неуспешное получение сокетов пользователя
+        Причина: пользователь не авторизован
+        """
+        self._logout()
+
         response = self.client.get(self.user_sockets_url)
 
         self.assertEqual(response.status_code, 401)
+
+    @parameterized.expand([
+        (
+            {
+                'username': 'user',
+                'email': 'user@gmail.com',
+                'password': 'userpassword',
+            },
+        ),
+    ])
+    def test_success_create_user(self, correct_user_data):
+        """
+        Успешное создание пользователя
+        """
+        response = self.client.post(
+            self.user_sockets_url,
+            correct_user_data,
+        )
+
+        self.assertEqual(get_user_model().objects.count(), 2)
+        self.assertEqual(response.status_code, 201)
+
+    @parameterized.expand([
+        (
+            {
+                'username': 'user',
+                'password': 'userPassword',
+            },
+        ),
+        (
+            {
+                'email': 'user@gmail.com',
+                'password': 'userPassword',
+            },
+        ),
+        (
+            {
+                'email': 'user@gmail.com',
+                'username': 'user',
+            },
+        ),
+    ])
+    def test_failure_create_user(self, incorrect_user_data):
+        """
+        Неудачное создание пользователя.
+        Причина: неполные данные
+        """
+        response = self.client.post(
+            self.user_sockets_url,
+            incorrect_user_data,
+        )
+
+        users_count = self._get_users_count()
+        self.assertEqual(users_count, 1)
+        self.assertEqual(response.status_code, 400)
